f1ffb1b9403fc04f1a355d613d801a704b78ec4b
Kishan Unnadkat <kunnadkat@Kishans-MacBook-Pro-2.local>
2017-11-13 12:33:41 +0000

    Working navigation menu with responsiveness

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+(function() {
+  
+  // Function that appends 'responsive' to the classname when hamburger menu has been clicked on
+  function clickHamburgerMenu() {
+    var navBar = document.getElementById("topNavigationBar");
+    if (navBar.className === "topNavigation") {
+      navBar.className += " responsive";
+    } else {
+      navBar.className = "topNavigation";
+    }
+  }
+  
+  // Add event listener so when responsive menu clicked on, runs clickHamburgerMenu function
+  document.getElementById('responsiveIcon').addEventListener("click", clickHamburgerMenu);
+})();
